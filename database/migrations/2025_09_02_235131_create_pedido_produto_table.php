a190050de9219eb2151c88ede86d15dda54c6934
Amanda Kelory <blaack.mind72@gmail.com>
2025-09-04 17:25:30 -0300

    migrations

---
+++ database/migrations/2025_09_02_235131_create_pedido_produto_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('pedido_produto', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pedidos_id')->constrained('pedidos')->onDelete('cascade');
+            $table->foreignId('produto_id')->constrained('produto')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedido_produto');
+    }
+};
